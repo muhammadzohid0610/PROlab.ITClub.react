e4bb1d479f80fa54eeb04b4b867d4ed0b759a656
abubakir <abukamaldinov2008@gmail.com>
2024-04-16 17:01:49 +0600

    oz

---
+++ src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AboutUs: React.FC = () => (
+	<h1>hellodedfrfre</h1>
+
+);
+
+export default AboutUs;
---
+++ src/components/Html In Css/Html.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Html: React.FC = () => (
+	<h1>hellodedfrfre</h1>
+
+);
+
+export default Html;
