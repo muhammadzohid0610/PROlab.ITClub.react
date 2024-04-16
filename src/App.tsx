bb21059484f521c3e0a17c36da514f98140bd4b3
abubakir <abukamaldinov2008@gmail.com>
2024-04-16 15:32:10 +0600

    ozgardi components

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import React from 'react';
 
 const App: React.FC = () => (
 	<h1>hellodedfrfre</h1>
-
 );
 
 export default App;
---
+++ src/components/SassBlog/Sass.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sass() {
+  return (
+	<div>Sass</div>
+  )
+}
+
+export default Sass
