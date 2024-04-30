7ea76e3dc78c0436715e775aa734ca4b4269fe75
Abubakir <abubakirnumandjanov@gmail.com>
2024-04-30 13:31:43 +0600

    qwerty

--- src/pages/Topics.tsx
+++ src/pages/Topics.tsx
@@ -5,7 +5,7 @@ const Topics: FC = () => {
   return (
     <div className="container mx-auto h-full flex justify-center">
       <div className="w-full my-auto bg-[red] h-full mt-[100px]">
-        <h1 className="text-[--black] text-[32px]">Topics:</h1>
+        <h1 className="text-[--black] text-[32px]">Topics: 1</h1>
       </div>
     </div>
   );
