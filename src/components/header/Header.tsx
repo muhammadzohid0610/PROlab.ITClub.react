edd518ffef75a47ee948a9cfd4f67aec4d2670f3
Muhammadzohid0610 <zohid061007@gmail.com>
2024-05-02 12:49:08 +0600

    asdasdsadd

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
-// components
 import Logo from "./Logo";
 import Translate from "../../Translate";
-
-// icons
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 import Set_Log from "./Set_Log";
@@ -32,26 +29,20 @@ const Header: React.FC<darkMode> = ({
   const [modal, setModal] = useState(false);
 
   return (
-    <div className="w-full bg-[--succesBg] navbar fixed backdrop-blur-sm top-[0] left-0 right-0 py-[5px] z-50">
-      <div className="container  mx-auto ">
-        <div className="px-[15px]  flex justify-between items-center">
-          <Logo />
+    <div className="container mx-auto fixed top-[0] left-0 right-0 mt-[30px] ">
+      <div className="px-[15px]  flex justify-between items-center">
+        <Logo />
 
-<<<<<<< HEAD
-          <div className="flex items-center gap-[30px]  text-[26px]">
-            <Set_Log modal={modal} setModal={setModal} />
-=======
         <div className="flex items-center gap-[30px]  text-[26px]">
           <Set_Log modal={modal} setModal={setModal} />
         </div>
       </div>
-
       <div
         onClick={() => setModal(!modal)}
         className={` ${
           modal ? "flex" : "hidden"
-        } w-[100vw]   h-[100vh] fixed top-0 z-[90] left-0 bg-[--succesBg] backdrop-blur-[4px]  justify-center items-center`}
-      >
+        } w-[100vw] h-[100vh] fixed top-0 z-[90] left-0 bg-[--succesBg] backdrop-blur-[4px]  justify-center items-center`}
+      > 
         <div
           className="w-[300px] xl:w-[500px] h-[500px] bg-[--white] rounded-[10px] flex justify-start px-[20px] py-[30px] flex-col  text-start items-start"
           onClick={(e) => e.stopPropagation()}
@@ -74,99 +65,64 @@ const Header: React.FC<darkMode> = ({
               </div>
               <Translate />
             </div>
->>>>>>> b7cc6e182c6367c1f6255eeea0fc3216b4abb007
           </div>
-        </div>
 
-        <div
-          onClick={() => setModal(!modal)}
-          className={` ${
-            modal ? "flex" : "hidden"
-          } w-[100vw]   h-[100vh] fixed top-0 z-[90] left-0 bg-[--succesBg] backdrop-blur-[4px]  justify-center items-center`}
-        >
-          <div
-            className="w-[300px] xl:w-[500px] h-[500px] bg-[--white] rounded-[10px] flex justify-start px-[20px] py-[30px] flex-col  text-start items-start"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <div className="gap-[10px] flex text-start items-start text-[22px] flex-col">
-              <h1 className="text-[--black] mt-[0px] text-[28px]">Settings:</h1>
-              <div className="flex  xl:flex-row  items-center gap-[12px] mt-[20px] text-start">
-                <div>
-                  {dark ? (
-                    <MdLightMode
-                      onClick={() => setDark(!dark)}
-                      className={`cursor-pointer text-[40px] xl:text-[80px] xl:border-r-2 xl:border-r-[--black] pr-[5px] mr-[2px]  text-[--black]`}
-                    />
-                  ) : (
-                    <MdDarkMode
-                      onClick={() => setDark(!dark)}
-                      className={`cursor-pointer text-[40px] xl:text-[80px] xl:border-r-2 xl:border-r-[--black] pr-[5px] mr-[2px]  text-[--black]`}
-                    />
-                  )}
-                </div>
+          <div className="w-full  flex flex-col gap-[20px]">
+            <h1 className="text-[--black] mt-[70px] text-[28px]">
+              Choose color:
+            </h1>
 
-                <Translate />
-              </div>
-            </div>
-
-            <div className="w-full  flex flex-col gap-[20px]">
-              <h1 className="text-[--black] mt-[70px] text-[28px]">
-                Choose color:
-              </h1>
-
-              <div className="flex justify-between mt-[20px]">
-                <div
-                  onClick={() => {
-                    setRed((red = true));
-                    setGreen((green = false));
-                    setYellow((yellow = false));
-                  }}
-                  className={`border-2  flex justify-center w-[70px] xl:w-[100px] rounded-[10px] py-[10px] hover:bg-[--black] cursor-pointer transition-all duration-300 ${
-                    red && "bg-[--black]"
-                  }`}
-                >
-                  <div className="w-[50px] h-[50px] bg-[red] border-[--black] rounded-[50%]"></div>
-                </div>
-
-                <div
-                  onClick={() => {
-                    setGreen((green = true));
-                    setRed((red = false));
-                    setYellow((yellow = false));
-                  }}
-                  className={`border-2  flex justify-center w-[70px] xl:w-[100px] rounded-[10px] py-[10px] hover:bg-[--black] cursor-pointer transition-all duration-300 ${
-                    green && "bg-[--black]"
-                  }`}
-                >
-                  <div className="w-[50px] h-[50px] bg-[green] border-[--black] rounded-[50%]"></div>
-                </div>
-
-                <div
-                  onClick={() => {
-                    setYellow((yellow = true));
-                    setGreen((green = false));
-                    setRed((red = false));
-                  }}
-                  className={`border-2  flex justify-center w-[70px] xl:w-[100px] rounded-[10px] py-[10px] hover:bg-[--black] cursor-pointer transition-all duration-300 ${
-                    yellow && "bg-[--black]"
-                  }`}
-                >
-                  <div className="w-[50px] h-[50px] bg-[#9d159d] border-[--black] rounded-[50%]"></div>
-                </div>
-              </div>
-              <button
+            <div className="flex justify-between mt-[20px]">
+              <div
                 onClick={() => {
-                  setRed((red = false));
+                  setRed((red = true));
                   setGreen((green = false));
                   setYellow((yellow = false));
                 }}
-                className={`${
-                  red || green || yellow ? "flex" : "hidden"
-                } text-[16px] text-[--black] border-2 border-[--black] w-max py-[7px] px-[20px] rounded-[10px] bg-[--succes]`}
+                className={`border-2  flex justify-center w-[70px] xl:w-[100px] rounded-[10px] py-[10px] hover:bg-[--black] cursor-pointer transition-all duration-300 ${
+                  red && "bg-[--black]"
+                }`}
               >
-                Delete changes
-              </button>
+                <div className="w-[50px] h-[50px] bg-[red] border-[--black] rounded-[50%]"></div>
+              </div>
+
+              <div
+                onClick={() => {
+                  setGreen((green = true));
+                  setRed((red = false));
+                  setYellow((yellow = false));
+                }}
+                className={`border-2  flex justify-center w-[70px] xl:w-[100px] rounded-[10px] py-[10px] hover:bg-[--black] cursor-pointer transition-all duration-300 ${
+                  green && "bg-[--black]"
+                }`}
+              >
+          <div className="w-[50px] h-[50px] bg-[green] border-[--black] rounded-[50%]"></div>
+              </div>
+              <div
+                onClick={() => {
+                  setYellow((yellow = true));
+                  setGreen((green = false));
+                  setRed((red = false));
+                }}
+                className={`border-2  flex justify-center w-[70px] xl:w-[100px] rounded-[10px] py-[10px] hover:bg-[--black] cursor-pointer transition-all duration-300 ${
+                  yellow && "bg-[--black]"
+                }`}
+              >
+                <div className="w-[50px] h-[50px] bg-[#9d159d] border-[--black] rounded-[50%]"></div>
+              </div>
             </div>
+            <button
+              onClick={() => {
+                setRed((red = false));
+                setGreen((green = false));
+                setYellow((yellow = false));
+              }}
+              className={`${
+                red || green || yellow ? "flex" : "hidden"
+              } text-[16px] text-[--black] border-2 border-[--black] w-max py-[7px] px-[20px] rounded-[10px] bg-[--succes]`}
+            >
+              Delete changess
+            </button>
           </div>
         </div>
       </div>
